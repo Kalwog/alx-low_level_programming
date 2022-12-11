d4c129008db4ddd4c1324dfc73d4508e7d551945
Kalwog <kaleabneway@gmail.com>
2022-12-11 13:55:11 -0800

    task 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - main function prit 0 to 9
+ *
+ * Return: 0 success
+ */
+
+int main(void)
+
+{
+int n;
+for (n = 0; n <= 9; n++)
+{
+putchar(n + '0');
+if (i < 9)
+{
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
+		return (0);
+}
