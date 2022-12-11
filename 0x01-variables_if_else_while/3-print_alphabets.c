390de12d6cef142481f9ec3336ffb871f799da43
Kalwog <kaleabneway@gmail.com>
2022-12-11 10:37:13 -0800

    task 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+
+
+/**
+ * main - main function prit a to z
+ *
+ * Return: 0 success
+ *
+ */
+
+int main(void)
+
+{
+
+			char c;
+
+			for (c = 'a'; c <= 'z'; c++)
+
+				putchar(c);
+
+			for (c = 'A'; c <= 'Z'; c++)
+
+				putchar(c);
+
+			putchar('\n');
+
+					return (0);
+
+}
