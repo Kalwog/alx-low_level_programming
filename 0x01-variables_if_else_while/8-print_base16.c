ecae698a9628f457701a614066b23ca211ab5a12
Kalwog <kaleabneway@gmail.com>
2022-12-11 12:10:55 -0800

    task 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - main function prit 0 to 15
+ *
+ * Return: 0 success
+ */
+
+int main(void)
+
+{
+int i;
+char c = 'A';
+for (i = 0; i < 16; i++)
+{
+if (num < 10)
+{
+putchar(i + '0');
+}
+else
+{
+putchar(c);
+c++;
+}
+putchar('\n');
+}
+		return (0);
+}
