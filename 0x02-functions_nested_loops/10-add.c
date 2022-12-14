e226fff4de3ed8b4c701ca8ffc25708e2ed926c2
Kalwog <kaleabneway@gmail.com>
2022-12-14 14:30:31 -0800

    task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - adds two integers
+ * @a: int added to a
+ * @b: int added to b
+ * Return: the result
+ */
+int add(int a, int b)
+{
+	int c;
+
+	c = a + b;
+
+	return (c);
+}
