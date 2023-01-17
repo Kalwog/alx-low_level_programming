a7a24883bc6f05208bf5897cfba50c523e9c30e0
Kalwog <kaleabneway@gmail.com>
2023-01-17 21:39:38 +0300

    task 2

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -2,25 +2,18 @@
 
 /**
  * _strchr - locates a character in a string
- *
  * @c: occurrence of the character
- *
  * @s: in the string
- *
  * Return: a pointer to the first occurrence of the character
  */
-
 char *_strchr(char *s, char c)
 {
-	while (*s)
-	{
-		if (*s != c)
-			s++;
-		else
-			return (s);
-	}
-	if (c == '\0')
-		return (s);
+	int i = 0;
 
-	return (NULL);
+	for (; s[i] >= '\0'; i++)
+{
+	if (s[i] == c)
+		return (&s[i]);
+}
+	return (0);
 }
